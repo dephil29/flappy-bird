ee2a9418d471ccb53e1e6c5c2c1cafe3b419a69e
Eric Phillips <Eric Phillips>
2017-05-13 15:31:12 -0400

    fixed stuff

--- js/Bird.js
+++ js/Bird.js
@@ -7,25 +7,66 @@ const Bird = function(x, y, ctx){
   this.height = 64;
   this.ticks = 0;
   this.spriteIndex = 0;
+  this.dead = false;
   this.sprites = [document.getElementById("bird1"),
       document.getElementById("bird2"),
       document.getElementById("bird3")];
   var self = this;
   window.addEventListener("keydown", function(e){
     if(e.keyCode === 32){
-      self.velY = -16;
+      self.velY = -12;
     }
   });
 };
-Bird.prototype.update = function(){
-  this.ticks++;
-  if(this.ticks % 15 ===0) this.spriteIndex = (this.spriteIndex)
+Bird.prototype.update = function(pipes){
   this.y += this.velY;
-  this.velY += 2;
+  this.velY += 1;
+  if(this.detectCollisions(pipes)){
+    this.dead = true;
+  };
+  if(this.dead) return;
+  this.ticks++;
+  if(this.ticks % 5 === 0) this.spriteIndex = (this.spriteIndex+1) % this.sprites.length;
 };
 
 Bird.prototype.render = function(){
-  let renderX = this.x - this.width/2;
-  let renderY = this.y - this.height/2;
+  let renderX = - this.width/2;
+  let renderY = - this.height/2;
+  this.ctx.save();
+  this.ctx.translate(this.x, this.y);
+  let angle = this.velY/32;
+  this.ctx.rotate(angle);
   this.ctx.drawImage(this.sprites[this.spriteIndex], renderX, renderY);
+
+  this.ctx.restore();
+};
+
+Bird.prototype.detectCollisions = function(pipes){
+  let collisionDetected = false;
+  for(var i = 0; i < pipes.length; i++){
+    let e = pipes[i];
+    let highPipe = e.ypos <= 0;
+    let x0 = e.xpos, x1 = e.xpos + e.width;
+    let a2 = this.x +44;
+    let b2 = this.y;
+    if(highPipe){
+      let y0 = e.ypos + e.length;
+      let a = this.x;
+      let b = this.y - this.height/2;
+      if (a > x0 && a < x1 && b < y0 ||
+          a2 > x0 && a2 < x1 && b2 < y0){
+        return true;
+      }
+    }
+    else {
+      let y2 = e.ypos;
+      let a = this.x;
+      let b = this.y + this.height/2;
+      if(a > x0 && a < x1 && b > y2 ||
+          a2 > x0 && a2 < x1 && b2 > y2){
+        return true;
+      }
+    }
+  };
+  return false;
 }
--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 
+
+
 window.onload = function() {
   const c = document.getElementById("canvas");
   c.width = window.innerWidth;
@@ -7,17 +9,45 @@ window.onload = function() {
   const ctx = c.getContext("2d");
 
   const environment = new Environment(c, ctx);
-  const bird = new Bird(100, 250, ctx);
+  const bird = new Bird(250, 300, ctx);
+  const pipes = [];
+  let pipeSet = generateRandomPipes(ctx, c.width, c.height);
+  pipes.push(pipeSet.top, pipeSet.bottom);
+  setInterval(function(){
+    let pipeSet = generateRandomPipes(ctx, c.width, c.height);
+    pipes.push(pipeSet.top, pipeSet.bottom)
+  }, 2600);
   gameLoop();
 
-ctx.fillStyle = "#FFFFFF";
 
 function gameLoop(){
   ctx.fillRect(0, 0, c.width, c.height);
   environment.update();
   environment.render();
-  bird.update();
+  pipes.forEach(function(pipe1){
+    pipe1.update();
+    pipe1.render();
+  });
+  bird.update(pipes);
   bird.render();
+  if(bird.dead){
+    drawGameOver(ctx, c);
+    return;
+  }
   window.requestAnimationFrame(gameLoop);
 }
 };
+
+function generateRandomPipes(ctx, canvasWidth, canvasHeight){
+  let lengthTop = Math.round(Math.random()*200+100);
+  let lengthBottom = canvasHeight - 200 - lengthTop;
+  let returnVal = { };
+  returnVal.top = new Pipe(canvasWidth, -5, lengthTop, 4, ctx);
+  returnVal.bottom = new Pipe(canvasWidth, canvasHeight+5-lengthBottom, lengthBottom, 4, ctx);
+  return returnVal;
+};
+
+function drawGameOver(ctx, c){
+  var img = document.getElementById("fail");
+  ctx.drawImage(img, 600, 150);
+}
