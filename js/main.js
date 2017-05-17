163f96a4932ecdff2aed9558ee30a762c182e5c4
Eric Phillips <Eric Phillips>
2017-05-17 16:11:56 -0400

    reload button, score counter, colors

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
-
-
-
 window.onload = function() {
   const c = document.getElementById("canvas");
   c.width = window.innerWidth;
   c.height = 600;
-
   const ctx = c.getContext("2d");
-
   const environment = new Environment(c, ctx);
   const bird = new Bird(250, 300, ctx);
   const pipes = [];
@@ -17,8 +12,16 @@ window.onload = function() {
     let pipeSet = generateRandomPipes(ctx, c.width, c.height);
     pipes.push(pipeSet.top, pipeSet.bottom)
   }, 2600);
+
   gameLoop();
 
+  var i = 0;
+  setInterval(function() {
+    if(i >= 0 && bird.dead === false){
+      i++;
+      document.getElementById("score").innerHTML = "Score: " + i;
+    }
+  }, 50);
 
 function gameLoop(){
   ctx.fillRect(0, 0, c.width, c.height);
@@ -34,6 +37,11 @@ function gameLoop(){
     drawGameOver(ctx, c);
     return;
   }
+  window.addEventListener("keydown", function(e){
+    if(bird.dead === true && e.keyCode === 13){
+      location.reload();
+    }
+  });
   window.requestAnimationFrame(gameLoop);
 }
 };
